0a8bddc3558688bbb79fcf95fdbb137499e98951
bliss <bliss@Rohits-MacBook-Air.local>
2023-09-09 19:38:07 +0530

    added react router and built few more components and fixed styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom/client";
 import Body from "./components/Body";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
@@ -16,4 +20,20 @@ const AppLayout = () => {
   );
 };
 
-root.render(<AppLayout />);
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/contact",
+    element: <Contact />,
+  },
+]);
+
+root.render(<RouterProvider router={appRouter} />);
